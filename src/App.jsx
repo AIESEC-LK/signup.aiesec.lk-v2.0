5d9efcb738b8d6306ac2c33ebd616e9357b7cac4
dilumin <77558016+dilumin@users.noreply.github.com>
2024-08-26 14:04:12 +0530

    project Initializationn

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        AIESEC in SRI LANKA
+      </div>
+    </>
+  )
+}
+
+export default App
