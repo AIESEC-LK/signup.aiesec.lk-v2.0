a43e0270f2c2221e0346e36b58b6052ca809024d
dilumin <77558016+dilumin@users.noreply.github.com>
2024-09-05 19:28:39 +0530

    sign up page update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import SignUp from "./pages/SignUp";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
-    <>
-      <div>
-        AIESEC in SRI LANKA
-      </div>
-    </>
-  )
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
