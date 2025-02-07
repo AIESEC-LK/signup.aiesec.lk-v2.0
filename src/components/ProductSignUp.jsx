f8e4bdd140688d048da71fe46ecb7e09cc59ab5e
Dev Team - AIESEC in Sri Lanka <147531381+asldevteam@users.noreply.github.com>
2025-02-07 11:52:41 +0530

    Update ProductSignUp.jsx - data fetch new url

--- src/components/ProductSignUp.jsx
+++ src/components/ProductSignUp.jsx
@@ -203,7 +203,7 @@ const ProductSignUp = (props) => {
     const serializedData = urlEncodedData.toString();
     const sendDataToSheet = async () => {
       const url =
-        "https://script.google.com/a/macros/aiesec.net/s/AKfycbwjGZu4InSGNujah-6JcNOn6MIhXEvy-c0gyl8oXKycrLvoFrxa5hW7QyFb09TRIgNnDw/exec";
+        "https://script.google.com/macros/s/AKfycbxzegVoLnMZQ8n-uChpy4ScxxTMKSwzbqOcdOdImcRD8mVTzntXMvHPE_xIFNF_SwfoBQ/exec";
         const urlObj = new URL(url);
         combinedData.url = urlObj.href; 
       
